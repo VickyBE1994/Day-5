c5648d4c72c2eb945c78589268e84c28a3833c9d
VickyBE1994 <vickyvip81@gmail.com>
2023-11-04 18:55:23 -0400

    Day 5 Task

---
+++ script.js
@@ -0,0 +1,44 @@
+var resume = { 
+    
+    "personal info":{
+    "name": "vignesh",
+    "label": "mechanical engnier",
+    "email": "vickyvip81@gmail.com",
+    "phone":"+91-9047443417"},
+    "SUMMARY": ["im newly entred in the INFORMATION TECHNOLOGY world"],
+    "LOCATION": {
+      "address": "1/49,sokkantheru ,maruthankon viduthi (po),karambakudi(tk)",
+      "postalCode": "622302",
+      "city": "pudukkottai",
+      "District":"pudukkottai",
+      "State":"Tamilnadu",
+     
+    },
+  
+  "EDUCATION": {
+    "institution": "Anna univercity",
+    "college":"Mahatma  college of Engineering",
+    "Degree": "BE",
+    "Duration": "2021-2025",
+    
+  },
+  
+  "LANGUAGES": {
+    "language": "Tamil ,English" },
+  
+
+  "INTEREST": {
+    "name": "Watching movie",
+    "keywords": 
+      "based on true storeies",
+    
+  },
+  "REFERENCE": {
+    "name": "selvam",
+    "reference": "parmasivan",
+    "phone":"+91-7708991995",
+  },
+}
+for(var i in resume){
+    console.log(i,resume[i]);
+}
